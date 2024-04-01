d3722b0beba7b009154cb16e51353bdd581daae0
zephyrus <joelogira@gmail.com>
2024-04-01 18:37:35 +0300

    Created navbar routes component

---
+++ components/NavbarRoutes.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { UserButton } from "@clerk/nextjs"
+import { usePathname } from "next/navigation"
+import { Button } from "../components/ui/button"
+import { LogOut } from "lucide-react"
+import Link from "next/link"
+
+export default function NavbarRoutes() {
+    const pathname = usePathname()
+
+    const isTeacherPage = pathname?.startsWith("/teacher")
+    const isPlayerPage = pathname?.includes("/chapter")
+
+    return (
+        <nav className="flex items-center gap-x-2 ml-auto">
+            {isTeacherPage || isPlayerPage ? (
+                <Link href="/">
+                    <Button size="sm" variant="outline">
+                        <LogOut className="w-4 h-4 mr-2" />
+                        Exit
+                    </Button>
+                </Link>
+            ) : (
+                <Link href="/teachers/courses" >
+                    <Button size="sm" variant="outline" >
+                        Teacher Mode
+                    </Button>
+                </Link>
+            )}
+
+            <UserButton
+                afterSignOutUrl="/"
+            />
+
+        </nav>
+    )
+}
