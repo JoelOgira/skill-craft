b98581d08385110759100301f4f4809fa3ccb83b
zephyrus <joelogira@gmail.com>
2024-04-01 18:36:19 +0300

    modified sidebar component styling

--- app/(dashboard)/_components/Sidebar.tsx
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarRoutes from "./SidebarRoutes"
 
 export default function Sidebar() {
     return (
-        <div className="h-full w-56 border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+        <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
             <div className="p-6">
                 <Logo />
             </div>
