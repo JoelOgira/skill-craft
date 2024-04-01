b70511c91d937e05232a2d5ad2d66ddc58112e69
zephyrus <joelogira@gmail.com>
2024-04-01 18:34:55 +0300

    Added a link to the logo

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image"
 import logo from '../../../public/logoipsum.svg'
+import Link from "next/link"
 
 export default function Logo() {
     return (
-        <Image
-            alt="Skill Craft Logo"
-            src={logo}
-            width={130}
-            height={130}
-        />
+        <Link href="/">
+            <Image
+                alt="Skill Craft Logo"
+                src={logo}
+                width={130}
+                height={130}
+            />
+        </Link>
     )
 }
