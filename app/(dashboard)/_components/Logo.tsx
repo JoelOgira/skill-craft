22a53d63138c6001fb0c488311db3089b24f0275
zephyrus <joelogira@gmail.com>
2024-04-10 23:20:50 +0300

    Updated the Logo component

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -1,16 +1,13 @@
 import Image from "next/image"
 import logo from '../../../public/logoipsum.svg'
-import Link from "next/link"
 
 export default function Logo() {
     return (
-        <Link href="/">
-            <Image
-                alt="Skill Craft Logo"
-                src={logo}
-                width={130}
-                height={130}
-            />
-        </Link>
+        <Image
+            alt="Skill Craft Logo"
+            src={logo}
+            width={130}
+            height={130}
+        />
     )
 }
