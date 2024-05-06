51d201ba64ac037b4449f4563d68d32387f1ce27
Jay <joelogira@gmail.com>
2024-05-07 00:15:22 +0300

    Added chapter actions and its functionalities

--- app/(dashboard)/(routes)/teachers/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teachers/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,11 +1,15 @@
 import { auth } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 import { db } from "@/lib/db"
-import Link from "next/link";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
-import { IconBadge } from "@/components/icon-badge";
+import Link from "next/link"
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react"
+import { IconBadge } from "@/components/icon-badge"
 import ChapterTitleForm from "./_components/chapter-title-form"
 import ChapterDescriptionForm from "./_components/chapter-description-form"
+import ChapterAccessForm from "./_components/chapter-access-form"
+import ChapterVideoForm from "./_components/chapter-video-form"
+import Banner from "@/components/ui/banner"
+import ChapterActions from "./_components/chapter-actions"
 
 export default async function ChapterIdPage({
     params
@@ -42,17 +46,25 @@ export default async function ChapterIdPage({
     const completedFields = requiredFields.filter(Boolean).length
     const completionText = `(${completedFields}/${totalFields})`
 
+    const isComplete = requiredFields.every(Boolean)
+
     return (
-        <main className="p-6 h-[100vh]">
-            <div className="flex items-center justify-between">
-                <div className="w-full">
-                    <Link
-                        href={`/teachers/courses/${params.courseId}`}
-                        className="flex items-center space-x-2 text-sm hover:opacity-75 transition mb-6"
-                    >
-                        <ArrowLeft className="h-4 w-4" />
-                        <span>Back to course setup</span>
-                    </Link>
+        <>
+            {!chapter?.isPublished && (
+                <Banner
+                    variant="warning"
+                    label="This chapter is not yet published. It will not be visible in the course."
+                />
+            )}
+            <main className="p-6">
+                <Link
+                    href={`/teachers/courses/${params.courseId}`}
+                    className="flex items-center space-x-2 text-sm hover:opacity-75 transition mb-6"
+                >
+                    <ArrowLeft className="h-4 w-4" />
+                    <span>Back to course setup</span>
+                </Link>
+                <div className="flex items-center justify-between">
                     <div className="flex items-center justify-between w-full">
                         <div className="flex flex-col space-y-2">
                             <h1 className="text-2xl font-medium">
@@ -63,81 +75,64 @@ export default async function ChapterIdPage({
                             </span>
                         </div>
                     </div>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-                <div className="space-y-4">
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge size={"default"} icon={LayoutDashboard} />
-                        <h2 className="text-lg">
-                            Customize your chapter
-                        </h2>
-                    </div>
-                    <ChapterTitleForm
-                        initialData={chapter}
-                        chapterId={params.chapterId}
+                    <ChapterActions
+                        disabled={!isComplete}
                         courseId={params.courseId}
-                    />
-                    <ChapterDescriptionForm
-                        initialData={chapter}
                         chapterId={params.chapterId}
-                        courseId={params.courseId}
+                        isPublished={chapter.isPublished}
                     />
-                    {/* <ImageForm
-                        initialData={course}
-                        courseId={course.id}
-                    /> */}
-                    {/* <CategoryForm
-                        initialData={course}
-                        courseId={course.id}
-                        options={categories.map((category) => ({
-                            label: category.name, value: category.id
-                        }))}
-                    /> */}
                 </div>
 
-                {/* <div className="space-y-6" >
-                    <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                    <div className="space-y-4">
                         <div className="flex items-center gap-x-2">
-                            <IconBadge size={"default"} icon={ListChecks} />
+                            <IconBadge size={"default"} icon={LayoutDashboard} />
                             <h2 className="text-lg">
-                                Course Chapters
+                                Customize your chapter
                             </h2>
                         </div>
-                        <ChaptersForm
-                            initialData={course}
-                            courseId={course.id}
+                        <ChapterTitleForm
+                            initialData={chapter}
+                            chapterId={params.chapterId}
+                            courseId={params.courseId}
                         />
+                        <ChapterDescriptionForm
+                            initialData={chapter}
+                            chapterId={params.chapterId}
+                            courseId={params.courseId}
+                        />
+                        <div>
+                            <div className="flex items-center gap-x-2">
+                                <IconBadge size={"default"} icon={Eye} />
+                                <h2 className="text-lg">
+                                    Access Settings
+                                </h2>
+                            </div>
+                            <ChapterAccessForm
+                                initialData={chapter}
+                                chapterId={params.chapterId}
+                                courseId={params.courseId}
+                            />
+                        </div>
                     </div>
 
-                    <div>
-                        <div className="flex items-center gap-x-2">
-                            <IconBadge size={"default"} icon={CircleDollarSign} />
-                            <h2 className="text-lg">
-                                Sell your course
-                            </h2>
+                    <div className="space-y-4" >
+                        <div>
+                            <div className="flex items-center gap-x-2">
+                                <IconBadge size={"default"} icon={Video} />
+                                <h2 className="text-lg">
+                                    Add a video
+                                </h2>
+                            </div>
+                            <ChapterVideoForm
+                                initialData={chapter}
+                                chapterId={params.chapterId}
+                                courseId={params.courseId}
+                            />
                         </div>
-                        <PriceForm
-                            initialData={course}
-                            courseId={course.id}
-                        />
                     </div>
-
-                    <div>
-                        <div className="flex items-center gap-x-2">
-                            <IconBadge size={"default"} icon={File} />
-                            <h2 className="text-lg">
-                                Resources and Attachments
-                            </h2>
-                        </div>
-                        <AttachmentForm
-                            initialData={course}
-                            courseId={course.id}
-                        />
-                    </div>
-                </div> */}
-            </div>
-        </main>
+                </div>
+            </main>
+        </>
     )
 }
