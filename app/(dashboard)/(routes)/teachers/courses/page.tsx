9cf208305366eef2c1bc6302b89672a33e3bfc4c
Jay <joelogira@gmail.com>
2024-05-10 01:57:51 +0300

    Updated the page

--- app/(dashboard)/(routes)/teachers/courses/page.tsx
+++ app/(dashboard)/(routes)/teachers/courses/page.tsx
@@ -1,14 +1,31 @@
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs"
+import { columns } from "./_components/columns"
+import { DataTable } from "./_components/data-table"
+import { redirect } from "next/navigation"
 
-export default function CoursesPage() {
+export default async function CoursesPage() {
+
+    const { userId } = auth()
+
+    if (!userId) {
+        return redirect(`/`)
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId: userId
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+    })
+
     return (
         <main className="p-6">
-            <Link href='/teachers/courses/create' >
-                <Button>
-                    New Course
-                </Button>
-            </Link>
+            <div className="container">
+                <DataTable columns={columns} data={courses} />
+            </div>
         </main>
     )
 }
