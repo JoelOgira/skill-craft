15ae8b9e64618ce2d15b9d4d179c926305fcc7e6
Jay <joelogira@gmail.com>
2024-05-12 18:24:34 +0300

    Created categories component

---
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { Category } from '@prisma/client'
+
+import {
+    FcEngineering,
+    FcFilmReel,
+    FcMultipleDevices,
+    FcMusic,
+    FcOldTimeCamera,
+    FcSalesPerformance,
+    FcSportsMode
+} from "react-icons/fc"
+
+import { IconType } from "react-icons"
+import CategoryItem from './category-item'
+
+type CategoriesProps = {
+    items: Category[]
+}
+
+const iconMap: Record<Category["name"], IconType> = {
+    "Music": FcMusic,
+    "Photography": FcOldTimeCamera,
+    "Filming": FcFilmReel,
+    "Accounting": FcSalesPerformance,
+    "Computer Science": FcMultipleDevices,
+    "Fitness": FcSportsMode,
+    "Engineering": FcEngineering
+}
+
+export default function Categories({ items }: CategoriesProps) {
+    return (
+        <div className='flex items-center gap-x-2 pb-2 overflow-x-auto'>
+            {items.map((item) => (
+                <CategoryItem
+                    key={item.id}
+                    label={item.name}
+                    icon={iconMap[item.name]}
+                    value={item.id}
+                />
+            ))}
+        </div>
+    )
+}
