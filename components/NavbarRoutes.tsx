e91b4c2b9c40fe8da8ce9e2dfbbd718043dc67fe
Jay <joelogira@gmail.com>
2024-05-12 18:26:05 +0300

    Added search input component

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -5,34 +5,43 @@ import { usePathname } from "next/navigation"
 import { Button } from "../components/ui/button"
 import { LogOut } from "lucide-react"
 import Link from "next/link"
+import SearchInput from "./search-input"
 
 export default function NavbarRoutes() {
     const pathname = usePathname()
 
     const isTeacherPage = pathname?.startsWith("/teacher")
     const isPlayerPage = pathname?.includes("/chapter")
+    const isSearchPage = pathname === "/search"
 
     return (
-        <nav className="flex items-center space-x-2 ml-auto">
-            {isTeacherPage || isPlayerPage ? (
-                <Link href="/">
-                    <Button size="sm" variant="outline">
-                        <LogOut className="w-4 h-4 mr-2" />
-                        Exit
-                    </Button>
-                </Link>
-            ) : (
-                <Link href="/teachers/courses" >
-                    <Button size="sm" variant="outline" >
-                        Teacher Mode
-                    </Button>
-                </Link>
+        <>
+            {isSearchPage && (
+                <div className="hidden md:block">
+                    <SearchInput />
+                </div>
             )}
+            <nav className="flex items-center space-x-2 ml-auto">
+                {isTeacherPage || isPlayerPage ? (
+                    <Link href="/">
+                        <Button size="sm" variant="outline">
+                            <LogOut className="w-4 h-4 mr-2" />
+                            Exit
+                        </Button>
+                    </Link>
+                ) : (
+                    <Link href="/teachers/courses" >
+                        <Button size="sm" variant="outline" >
+                            Teacher Mode
+                        </Button>
+                    </Link>
+                )}
 
-            <UserButton
-                afterSignOutUrl="/"
-            />
+                <UserButton
+                    afterSignOutUrl="/"
+                />
 
-        </nav>
+            </nav>
+        </>
     )
 }
