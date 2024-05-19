427341decbfb6849025a6ad3a7cdcc5f8b7357c9
Jay <joelogira@gmail.com>
2024-05-19 23:57:24 +0300

    Updated the search page, adding courses list

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,12 +1,36 @@
+import { auth } from "@clerk/nextjs"
 import { db } from "@/lib/db"
 import Categories from "./_components/categories"
 import SearchInput from "@/components/search-input"
+import { getCourses } from "@/actions/get-courses"
+import { redirect } from "next/navigation"
+import CoursesList from "@/components/courses-list"
 
-export default async function SearchPage() {
+type SearchPageProps = {
+    searchParams: {
+        title: string;
+        categoryId: string
+    }
+}
+
+export default async function SearchPage({
+    searchParams
+}: SearchPageProps) {
+    const { userId } = auth()
+
+    if (!userId) {
+        redirect("/")
+    }
+
     const categories = await db.category.findMany({
         orderBy: {
             name: "asc"
         }
+    })
+
+    const courses = await getCourses({
+        userId,
+        ...searchParams
     })
 
     return (
@@ -14,10 +38,11 @@ export default async function SearchPage() {
             <div className="px-6 pt-6 md:hidden md:mb-0 block">
                 <SearchInput />
             </div>
-            <main className="p-6">
+            <main className="p-6 space-y-4">
                 <Categories
                     items={categories}
                 />
+                <CoursesList items={courses} />
             </main>
         </>
     )
